386f5d1a6c96ca0175423d82faefca34ce672ebd
“Bjarne <bjarne@briis.com>
2020-05-24 08:36:43 +0000

    Added Config Flow

---
+++ custom_components/unifiprotect/config_flow.py
@@ -0,0 +1,105 @@
+""" Config Flow to configure Unifi Protect Integration. """
+import logging
+
+from pyunifiprotect import UpvServer, NotAuthorized, NvrError
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import (
+    CONF_ID,
+    CONF_HOST,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_PORT,
+)
+
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from aiohttp import CookieJar
+
+from .const import (
+    DOMAIN,
+    DEFAULT_PORT,
+    CONF_IR_ON,
+    CONF_IR_OFF,
+    TYPE_IR_AUTO,
+    TYPE_IR_OFF,
+    TYPES_IR_OFF,
+    TYPES_IR_ON,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@config_entries.HANDLERS.register(DOMAIN)
+class UnifiProtectFlowHandler(ConfigFlow):
+    """Handle a Unifi Protect config flow."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def _show_setup_form(self, errors=None):
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                    vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Optional(CONF_IR_ON, default=TYPE_IR_AUTO): vol.In(TYPES_IR_ON),
+                    vol.Optional(CONF_IR_OFF, default=TYPE_IR_OFF): vol.In(
+                        TYPES_IR_OFF
+                    ),
+                }
+            ),
+            errors=errors or {},
+        )
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initiated by the user."""
+        if user_input is None:
+            return await self._show_setup_form(user_input)
+
+        errors = {}
+
+        session = async_create_clientsession(
+            self.hass, cookie_jar=CookieJar(unsafe=True)
+        )
+
+        unifiprotect = UpvServer(
+            session,
+            user_input[CONF_HOST],
+            user_input[CONF_PORT],
+            user_input[CONF_USERNAME],
+            user_input[CONF_PASSWORD],
+        )
+
+        try:
+            unique_id = await unifiprotect.unique_id()
+        except NotAuthorized:
+            errors["base"] = "connection_error"
+            return await self._show_setup_form(errors)
+        except NvrError:
+            errors["base"] = "nvr_error"
+            return await self._show_setup_form(errors)
+
+        entries = self._async_current_entries()
+        for entry in entries:
+            if entry.data[CONF_ID] == unique_id:
+                return self.async_abort(reason="server_exists")
+
+        return self.async_create_entry(
+            title=unique_id,
+            data={
+                CONF_ID: unique_id,
+                CONF_HOST: user_input[CONF_HOST],
+                CONF_PORT: user_input[CONF_PORT],
+                CONF_USERNAME: user_input.get(CONF_USERNAME),
+                CONF_PASSWORD: user_input.get(CONF_PASSWORD),
+                CONF_IR_ON: user_input.get(CONF_IR_ON),
+                CONF_IR_OFF: user_input.get(CONF_IR_OFF),
+            },
+        )
---
+++ custom_components/unifiprotect/const.py
@@ -0,0 +1,60 @@
+""" Constant definitions for Unifi Protect Integration."""
+
+from homeassistant.components.camera import DOMAIN as CAMERA_DOMAIN
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
+
+DOMAIN = "unifiprotect"
+UNIQUE_ID = "unique_id"
+
+ENTITY_ID_CAMERA_FORMAT = CAMERA_DOMAIN + "." + "{}_{}"
+ENTITY_ID_SENSOR_FORMAT = SENSOR_DOMAIN + "." + "{}_{}"
+ENTITY_ID_SWITCH_FORMAT = SWITCH_DOMAIN + "." + "{}_{}"
+ENTITY_ID_BINARY_SENSOR_FORMAT = BINARY_SENSOR_DOMAIN + "." + "{}_{}"
+ENTITY_UNIQUE_ID = DOMAIN + "_{}_{}"
+ENTITY_UNIQUE_ID2 = DOMAIN + "_{}_{}"
+
+ATTR_CAMERA_ID = "camera_id"
+ATTR_CAMERA_TYPE = "camera_type"
+ATTR_UP_SINCE = "up_since"
+ATTR_ONLINE = "online"
+ATTR_EVENT_SCORE = "event_score"
+
+CONF_THUMB_WIDTH = "image_width"
+CONF_RECORDING_MODE = "recording_mode"
+CONF_IR_MODE = "ir_mode"
+CONF_IR_ON = "ir_on"
+CONF_IR_OFF = "ir_off"
+
+DEFAULT_PORT = 7443
+DEFAULT_ATTRIBUTION = "Data provided by Ubiquiti's Unifi Protect Server"
+DEFAULT_BRAND = "Ubiquiti"
+DEFAULT_THUMB_WIDTH = 640
+
+DEVICE_CLASS_DOORBELL = "doorbell"
+
+TYPE_RECORD_MOTION = "motion"
+TYPE_RECORD_ALLWAYS = "always"
+TYPE_RECORD_NEVER = "never"
+TYPE_IR_AUTO = "auto"
+TYPE_IR_ON = "always_on"
+TYPE_IR_LED_OFF = "led_off"
+TYPE_IR_OFF = "always_off"
+
+TYPES_IR_OFF = [
+    TYPE_IR_OFF,
+    TYPE_IR_LED_OFF,
+]
+
+TYPES_IR_ON = [
+    TYPE_IR_AUTO,
+    TYPE_IR_ON,
+]
+
+UNIFI_PROTECT_PLATFORMS = [
+    "camera",
+    "binary_sensor",
+    "sensor",
+    "switch",
+]

0ccfc6aa3f2554213ab889b80c49919b472ab885
Christopher Bailey <cbailey@mort.is>
2021-12-04 15:05:52 -0500

    Improves service calls

--- custom_components/unifiprotect/services.py
+++ custom_components/unifiprotect/services.py
@@ -6,7 +6,9 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.service import async_extract_referenced_entity_ids
+from pydantic import ValidationError
 from pyunifiprotect.api import ProtectApiClient
+from pyunifiprotect.exceptions import BadRequest
 
 from .const import CONF_DURATION, CONF_MESSAGE, DOMAIN
 from .models import UnifiProtectEntryData
@@ -22,34 +24,46 @@ def _async_all_ufp_instances(hass: HomeAssistant) -> list[ProtectApiClient]:
     ]
 
 
+def _async_get_macs_for_device(device_entry: dr.DeviceEntry) -> list[str]:
+    return [
+        # MAC addresses in UFP are always caps
+        cval.replace(":", "").upper()
+        for ctype, cval in device_entry.connections
+        if ctype == dr.CONNECTION_NETWORK_MAC
+    ]
+
+
+def _async_get_ufp_instances(
+    hass: HomeAssistant, device_id: str
+) -> tuple[dr.DeviceEntry, ProtectApiClient]:
+    device_registry = dr.async_get(hass)
+    if not (device_entry := device_registry.async_get(device_id)):
+        raise HomeAssistantError(f"No device found for device id: {device_id}")
+
+    if device_entry.via_device_id is not None:
+        return _async_get_ufp_instances(hass, device_entry.via_device_id)
+
+    macs = _async_get_macs_for_device(device_entry)
+    ufp_instances = [
+        i for i in _async_all_ufp_instances(hass) if i.bootstrap.nvr.mac in macs
+    ]
+
+    if not ufp_instances:
+        raise HomeAssistantError(
+            f"No UniFi Protect NVR found for device ID: {device_id}"
+        )
+
+    return device_entry, ufp_instances[0]
+
+
 def _async_get_protect_from_call(
     hass: HomeAssistant, call: ServiceCall
-) -> list[tuple[str, ProtectApiClient]]:
+) -> list[tuple[dr.DeviceEntry, ProtectApiClient]]:
     referenced = async_extract_referenced_entity_ids(hass, call)
-    device_registry = dr.async_get(hass)
 
-    instances: list[tuple[str, ProtectApiClient]] = []
+    instances: list[tuple[dr.DeviceEntry, ProtectApiClient]] = []
     for device_id in referenced.referenced_devices:
-        if not (device_entry := device_registry.async_get(device_id)):
-            raise HomeAssistantError(f"No device found for device id: {device_id}")
-
-        name = str(device_entry.name_by_user or device_entry.name)
-        macs = [
-            # MAC addresses in UFP are always caps
-            cval.replace(":", "").upper()
-            for ctype, cval in device_entry.connections
-            if ctype == dr.CONNECTION_NETWORK_MAC
-        ]
-        ufp_instances = [
-            (name, i)
-            for i in _async_all_ufp_instances(hass)
-            if i.bootstrap.nvr.mac in macs
-        ]
-        if not ufp_instances:
-            raise HomeAssistantError(
-                f"No UniFi Protect NVR found for device ID: {device_id}"
-            )
-        instances += ufp_instances
+        instances.append(_async_get_ufp_instances(hass, device_id))
 
     return instances
 
@@ -57,30 +71,54 @@ def _async_get_protect_from_call(
 async def add_doorbell_text(hass: HomeAssistant, call: ServiceCall) -> None:
     message: str = call.data[CONF_MESSAGE]
     instances = _async_get_protect_from_call(hass, call)
-    await asyncio.gather(
-        *(i.bootstrap.nvr.add_custom_doorbell_message(message) for _, i in instances)
-    )
+
+    try:
+        await asyncio.gather(
+            *(
+                i.bootstrap.nvr.add_custom_doorbell_message(message)
+                for _, i in instances
+            )
+        )
+    except (BadRequest, ValidationError) as err:
+        raise HomeAssistantError(str(err)) from err
 
 
 async def remove_doorbell_text(hass: HomeAssistant, call: ServiceCall) -> None:
     message: str = call.data[CONF_MESSAGE]
     instances = _async_get_protect_from_call(hass, call)
-    await asyncio.gather(
-        *(i.bootstrap.nvr.remove_custom_doorbell_message(message) for _, i in instances)
-    )
+
+    try:
+        await asyncio.gather(
+            *(
+                i.bootstrap.nvr.remove_custom_doorbell_message(message)
+                for _, i in instances
+            )
+        )
+    except (BadRequest, ValidationError) as err:
+        raise HomeAssistantError(str(err)) from err
 
 
 async def set_default_doorbell_text(hass: HomeAssistant, call: ServiceCall) -> None:
     message: str = call.data[CONF_MESSAGE]
     instances = _async_get_protect_from_call(hass, call)
-    await asyncio.gather(
-        *(i.bootstrap.nvr.set_default_doorbell_message(message) for _, i in instances)
-    )
+
+    try:
+        await asyncio.gather(
+            *(
+                i.bootstrap.nvr.set_default_doorbell_message(message)
+                for _, i in instances
+            )
+        )
+    except (BadRequest, ValidationError) as err:
+        raise HomeAssistantError(str(err)) from err
 
 
 async def profile_ws(hass: HomeAssistant, call: ServiceCall) -> None:
     duration: int = call.data[CONF_DURATION]
     instances = _async_get_protect_from_call(hass, call)
     await asyncio.gather(
-        *(profile_ws_messages(hass, i, duration, name) for name, i in instances)
+        *(
+            profile_ws_messages(hass, i, duration, device_entry)
+            for device_entry, i in instances
+        )
     )
